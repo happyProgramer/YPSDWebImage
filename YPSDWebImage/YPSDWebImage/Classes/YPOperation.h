a4a1eaf00df3b827cff1d45f0b7d633e8c407efa
manager <manager@163.com>
2016-06-12 09:33:40 +0800

    02.增加了一个类
    02.增加了一个类

---
+++ YPSDWebImage/YPSDWebImage/Classes/YPOperation.h
@@ -0,0 +1,15 @@
+//
+//  YPOperation.h
+//  YPSDWebImage
+//
+//  Created by 宠爱 on 16/6/12.
+//  Copyright © 2016年 iscast. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface YPOperation : NSOperation
+
+-(void)beginOperation;
+
+@end
---
+++ YPSDWebImage/YPSDWebImage/Classes/YPOperation.m
@@ -0,0 +1,18 @@
+//
+//  YPOperation.m
+//  YPSDWebImage
+//
+//  Created by 宠爱 on 16/6/12.
+//  Copyright © 2016年 iscast. All rights reserved.
+//
+
+#import "YPOperation.h"
+
+@implementation YPOperation
+
+
+-(void)beginOperation{
+
+    NSLog(@"假装已经开始操作");
+}
+@end
