394e46b6d1ee70df49cbf83650242f4a21534639
manager <manager@163.com>
2016-06-12 09:25:39 +0800

    01.初始化了项目
    01.初始化了项目

---
+++ YPSDWebImage/YPSDWebImage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YPSDWebImage
+//
+//  Created by 宠爱 on 16/6/12.
+//  Copyright © 2016年 iscast. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ YPSDWebImage/YPSDWebImage/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  YPSDWebImage
+//
+//  Created by 宠爱 on 16/6/12.
+//  Copyright © 2016年 iscast. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
