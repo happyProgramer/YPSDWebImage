c31e8f46c56206cbd11889668f1dcc6fba772ec8
manager <manager@163.com>
2016-06-12 09:35:04 +0800

    03.初始化一个操作，并执行其方法
    03.初始化一个操作，并执行其方法

--- YPSDWebImage/YPSDWebImage/ViewController.m
+++ YPSDWebImage/YPSDWebImage/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "YPOperation.h"
 
 @interface ViewController ()
 
@@ -16,7 +17,11 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
+    YPOperation *operation = [[YPOperation alloc] init];
+    
+    [operation beginOperation];
+    
 }
 
 - (void)didReceiveMemoryWarning {
